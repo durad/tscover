9369a9fbe5d6174fa0c4564bfb6e7e2dd1435b98
Dusan Radovanovic <dushanr@gmail.com>
2016-12-12 00:35:35 -0800

    Work in progress.

--- src/tscover.ts
+++ src/tscover.ts
@@ -11,11 +11,10 @@ let tscCode: string = fs.readFileSync(tscPath, 'utf8');
 tscCode = tscCode.replace(/ts.executeCommandLine\(ts\.sys\.args\)\;/, '// ts.executeCommandLine(ts.sys.args);');
 tscCode = '(function(require, __filename) { ' + tscCode + ' ;return ts; })';
 
-let ts = vm.runInThisContext(tscCode, { displayErrors: true })(require, tscPath);
-let createProgramOriginal = ts.createProgram;
-
 let sk = vm.runInThisContext('(function(typescript) { return typescript.SyntaxKind })')(typescript);
 
+let ts = vm.runInThisContext(tscCode, { displayErrors: true })(require, tscPath);
+let createProgramOriginal = ts.createProgram;
 
 
 
@@ -32,146 +31,6 @@ function whitespace(length: number, ch: string = ' ') {
 	for (let i = 0; i < length; i++) s.push(ch);
 
 	return s.join('');
-}
-
-class TreeVisitor {
-	sourceFile: typescript.SourceFile;
-	rootVisitor: NodeVisitor;
-	relPath: string;
-	statementCount: number = 0;
-
-	coverage = {
-		path: '',
-		s: {},
-		b: {},
-		f: {},
-		fnMap: {},
-		statementMap: {},
-		branchMap: {}
-	};
-
-	constructor(sourceFile: typescript.SourceFile, relPath: string) {
-		this.sourceFile = sourceFile;
-		this.coverage.path = this.sourceFile.fileName;
-		this.rootVisitor = new NodeVisitor(this, this.sourceFile, null, 0);
-		this.relPath = relPath;
-	}
-
-	visit(): string {
-		return this.rootVisitor.visit();
-	}
-
-	reportStatement(start: number, length: number): string {
-		let startPos = this.sourceFile.getLineAndCharacterOfPosition(start);
-		let endPos = this.sourceFile.getLineAndCharacterOfPosition(start + length);
-
-		this.statementCount++;
-
-		this.coverage.statementMap[this.statementCount + ''] = {
-			start: {
-				line: startPos.line + 1,
-				column: startPos.character
-			},
-			end: {
-				line: endPos.line + 1,
-				column: endPos.character
-			}
-		};
-
-		this.coverage.s[this.statementCount + ''] = 0;
-
-		return `__coverage__.s['${this.statementCount}']++`;
-	}
-}
-
-class NodeVisitor {
-	treeVisitor: TreeVisitor;
-	node: typescript.Node;
-	parent: typescript.Node;
-	depth: number;
-	children: typescript.Node[];
-	isLeaf: boolean;
-	syntaxKindName: string;
-	whitespace: string;
-	nodeText: string;
-	childCode: string;
-
-	constructor(treeVisitor, node: typescript.Node, parent: typescript.Node, depth: number = 0) {
-		this.treeVisitor = treeVisitor;
-		this.node = node;
-		this.parent = parent;
-		this.depth = depth;
-		this.children = node.getChildren();
-		this.isLeaf = this.children.length === 0;
-		this.syntaxKindName = sk[this.node.kind];
-		this.nodeText = this.node.getFullText();
-		this.whitespace = whitespace(depth, ' ');
-	}
-
-	ntChildCode(): string {
-		if (this.childCode === undefined) this.childCode = this.visitChildren();
-
-		return this.childCode.substring(this.node.getLeadingTriviaWidth(), this.childCode.length);
-	}
-
-	visit(): string {
-
-		let cc = '';
-		let tw = this.node.getLeadingTriviaWidth();
-		let start = this.node.getFullStart() + tw;
-		let width = this.node.getFullWidth() - tw;
-
-		let path = this.treeVisitor.relPath;
-
-		switch (this.node.kind) {
-
-			// case typescript.SyntaxKind.IfStatement:
-			// 	let ifStatement = this.node as typescript.IfStatement;
-			// 	cc = this.node.getFullText();
-
-			// 	return `${cc.substring(0, tw)}if (${this.visitNode(ifStatement.expression)}) ` + 
-			// 		`{ __coverage__.THEN; ${this.visitNode(ifStatement.thenStatement)} } ` +
-			// 		`else { __coverage__.ELSE; ${this.visitNode(ifStatement.elseStatement)} }`;
-
-			// case typescript.SyntaxKind.ForOfStatement:
-			// 	let s = this.node as typescript.ForOfStatement;
-			// 	cc = this.visitChildren();
-			// 	this.treeVisitor.reportLocation(start, width);
-			// 	return `${cc.substring(0, tw)}__coverage__.C('${path}', ${start}, ${width});${this.ntChildCode()}`;
-
-			case sk.CallExpression:
-				cc = this.visitChildren();
-				let locInc = this.treeVisitor.reportStatement(start, width);
-				return `${cc.substring(0, tw)}(${locInc}, ${this.ntChildCode()})`;
-
-			// case typescript.SyntaxKind.BinaryExpression:
-			// 	this.treeVisitor.reportLocation(start, width);
-			// 	return `__coverage__.C('${path}', ${start}, ${width}, ${this.visitChildren()})`;
-
-			default: return this.visitChildren();
-		}
-	}
-
-	visitChildren(): string {
-		if (this.isLeaf) return this.nodeText;
-
-		let children = this.children;
-		let p = [];
-		for (let child of children) {
-			let childVisit = new NodeVisitor(this.treeVisitor, child, this.node, this.depth + 1);
-			let c = childVisit.visit();
-			p.push(c);
-		}
-
-		return p.join('');
-	}
-
-	visitNode(node: typescript.Node): string {
-		if (!node) return '';
-
-		let nodeVisit = new NodeVisitor(this.treeVisitor, node, this.node, this.depth + 1);
-		return nodeVisit.visit();
-	}
 }
 
 
@@ -193,9 +52,11 @@ ts.createProgram = function(fileNames, compilerOptions, compilerHost): typescrip
 		if (fileName.match(/\.ts$/) && !fileName.match(/\.d\.ts$/) && source) {
 
 			console.log(fileName);
-			let t = new TreeVisitor(source, fileName);
-			let instrumentedSource = t.visit();
-			instrumentedSource = 'let __coverage__ : any={};\n\n' + instrumentedSource;
+			// let t = new TreeVisitor(source, fileName);
+			// let instrumentedSource = t.visit();
+			// instrumentedSource = 'let __coverage__ : any={};\n\n' + instrumentedSource;
+
+			let instrumentedSource = visitSource(source);
  
 			return ts.createSourceFile(fileName, instrumentedSource, languageVersion);
 		}
@@ -209,7 +70,104 @@ ts.createProgram = function(fileNames, compilerOptions, compilerHost): typescrip
 	return program;
 };
 
+
+function visitChildren(node: typescript.Node, depth: number): string {
+	let children = node.getChildren();
+	let nodeText = node.getFullText();
+
+	if (children.length === 0) return nodeText;
+
+	let p = [];
+	for (let child of children) {
+		let childVisit = visitNode(child, node, depth + 1);
+		p.push(childVisit);
+	}
+
+	return p.join('');
+}
+
+function visitNode(node: typescript.Node, parent: typescript.Node, depth: number): string {
+	let cc = '';
+	let tw = node.getLeadingTriviaWidth();
+	let start = node.getFullStart() + tw;
+	let width = node.getFullWidth() - tw;
+
+	// let path = this.treeVisitor.relPath;
+
+	// console.log(whitespace(depth * 2, ' ') + depth + '  ' + node.getChildCount() + ' ' + sk[node.kind]);
+	console.log(whitespace(depth * 2, ' ') + sk[node.kind]);
+
+	switch (node.kind) {
+
+		case sk.ClassDeclaration:
+			cc = visitChildren(node, depth);
+			return `${cc.substring(0, tw)}/*CLASS++*/${cc.substring(tw)}`;
+
+		case sk.FunctionDeclaration:
+			cc = visitChildren(node, depth);
+			return `${cc.substring(0, tw)}/*FUNC++*/${cc.substring(tw)}`;
+
+		case sk.IfStatement:
+			// cc = visitChildren(node, depth);
+			// return `${cc.substring(0, tw)}/*if++*/${cc.substring(tw)}`;
+
+			let ifStatement = node as typescript.IfStatement;
+			cc = node.getFullText();
+
+			return `${cc.substring(0, tw)}if (${visitNode(ifStatement.expression, node, depth)}) ` + 
+				`{/*THEN++;*/ ${visitNode(ifStatement.thenStatement, node, depth)} } ` +
+				`else {/*ELSE++;*/ ${visitNode(ifStatement.elseStatement, node, depth)} }`;
+
+			// case typescript.SyntaxKind.ForOfStatement:
+			// 	let s = this.node as typescript.ForOfStatement;
+			// 	cc = this.visitChildren();
+			// 	this.treeVisitor.reportLocation(start, width);
+			// 	return `${cc.substring(0, tw)}__coverage__.C('${path}', ${start}, ${width});${this.ntChildCode()}`;
+
+		case sk.CallExpression:
+
+			cc = visitChildren(node, depth);
+			//let locInc = this.treeVisitor.reportStatement(start, width);
+			// return `${cc.substring(0, tw)}(${123}, ${cc.substring(tw)})`;
+			return `${cc.substring(0, tw)}/*CALL++*/${cc.substring(tw)}`;
+
+		// case typescript.SyntaxKind.BinaryExpression:
+		// 	this.treeVisitor.reportLocation(start, width);
+		// 	return `__coverage__.C('${path}', ${start}, ${width}, ${this.visitChildren()})`;
+
+		// case sk.FirstAssignment:
+		// 	// let binaryExpression = node as typescript.;
+		// 	// console.log(binaryExpression); 
+		// 	cc = visitChildren(node, depth);
+		// 	return `${cc.substring(0, tw)}/*ASSIGN++*/${cc.substring(tw)}`;
+
+		// case sk.BinaryExpression:
+		// 	let binaryExpression = node as typescript.BinaryExpression;
+		// 	// console.log('----------');
+		// 	// console.log(binaryExpression.getFullText());
+		// 	// console.log(sk[binaryExpression.operatorToken.kind]);
+
+		// 	if (binaryExpression.operatorToken.kind == sk.FirstAssignment ||
+		// 		binaryExpression.operatorToken.kind == sk.FirstCompoundAssignment) {
+		// 		// if (binaryExpression.) 
+		// 		cc = visitChildren(node, depth);
+		// 		return `${cc.substring(0, tw)}/*EXPRESS++*/${cc.substring(tw)}`;
+		// 	}
+
+		case sk.LetKeyword:
+			cc = visitChildren(node, depth);
+			return `${cc.substring(0, tw)}/*VAR++*/${cc.substring(tw)}`;
+
+		default: //return visitChildren(node, depth);
+			return visitChildren(node, depth);
+	}
+}
+
+function visitSource(source: typescript.SourceFile): string {
+	let r = visitChildren(source, 0);
+	console.log(r);
+	return r;
+}
+
 ts.executeCommandLine(ts.sys.args);
-
-
 
--- tests/tssample1/main.ts
+++ tests/tssample1/main.ts
@@ -7,4 +7,29 @@ let l = new Lib(346);
 l.someFunction();
 l.someFunction();
 
+function a() {};
+function b(cb: () => any) {};
+
+a();
+l.someFunction();
+b(() => {
+    a();
+    l.someFunction();
+
+    let q: any;
+    let y: number = 22, u: number = 44;
+    q = 123;
+    q = q + 1;
+    q++;
+    q = new Lib(234);
+    q = null;
+    q = undefined;
+    q += 2;
+});
+
+let x = 123;
+
+if (x % 3 == 0) {
+    console.log('Fizz');
+} else console.log('Buzz');
 

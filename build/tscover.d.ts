0c1e061b16575c11068e5d01e4f17384944cb892
Dusan Radovanovic <dushanr@gmail.com>
2017-04-10 11:57:58 -0700

    Minor changes in definitions

--- build/tscover.d.ts
+++ build/tscover.d.ts
@@ -2,7 +2,7 @@
 interface TSCover {
 	/**
 	 * Gets coverage data from current session in LCOV format.
-	 * @param pathRepam If present its `from` and `to` properties are used to remap file paths in LCOV file.
+	 * @param lcovPath If present its `from` and `to` properties are used to remap file paths in LCOV file.
 	 * @returns LCOV data as a string.
 	 */
 	generateLcov(pathRemap?: { from: string | RegExp, to: string }): string;
@@ -10,12 +10,12 @@ interface TSCover {
 	/**
 	 * Saves coverage data in LCOV format to disk.
 	 * @param lcovPath Path of the LCOV file. Defaults to lcov.info.
-	 * @param pathRepam If present its `from` and `to` properties are used to remap file paths in LCOV file.
+	 * @param lcovPath If present its `from` and `to` properties are used to remap file paths in LCOV file.
 	 */
-	saveLcov(lcovPath: string, pathRemap?: { from: string, to: string });
+	saveLcov(lcovPath?: string, pathRemap?: { from: string, to: string });
 }
 
 /**
- * Global coverate object used access code coverage functionality.
+ * Global coverage object used access code coverage functionality.
  */
-export declare var __coverage__: TSCover;
+declare const __coverage__: TSCover;

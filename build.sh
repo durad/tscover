768610ed5915b0a5bc65d20dbe4ad3a9a2f779fd
Dusan Radovanovic <dushanr@gmail.com>
2017-11-13 15:09:28 -0800

    Running tests with ts-node

--- build.sh
+++ build.sh
@@ -47,7 +47,6 @@ export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $( mkdir -p "${OUTDIR}" )
 export OUTDIR_NORM="`cd "${OUTDIR}";pwd`"
 export SRC_NORM="`cd "${SCRIPT_DIR}";cd "./src";pwd`"
-export TEST_NORM="`cd "${SCRIPT_DIR}";cd "./test";pwd`"
 export HEADER_NORM="`cd "${SCRIPT_DIR}";cd "./header";pwd`"
 
 echo "OUTDIR_NORM=$OUTDIR_NORM"
@@ -58,16 +57,8 @@ SRC_CMD="tsc --pretty"
 if [[ $WATCH == true ]]; then
 	SRC_CMD="$SRC_CMD --watch"
 fi
-SRC_CMD="$SRC_CMD --project \"$SRC_NORM\" --outDir \"$OUTDIR_NORM\" "
+SRC_CMD="$SRC_CMD --outDir \"$OUTDIR_NORM\" "
 # echo "SRC_CMD=$SRC_CMD"
-
-
-TEST_CMD="tsc --pretty"
-if [[ $WATCH == true ]]; then
-	TEST_CMD="$TEST_CMD --watch"
-fi
-TEST_CMD="$TEST_CMD --project \"$TEST_NORM\" --outDir \"$TEST_NORM\" "
-# echo "TEST_CMD=$TEST_CMD"
 
 
 HEADER_CMD="node $HEADER_NORM/buildheader.js --outDir \"$OUTDIR_NORM\" "
@@ -83,26 +74,20 @@ if [[ $WATCH == false ]]; then
 	# echo "Compiling src:"
 	echo "$SRC_CMD"
 	eval "$SRC_CMD"
-	# echo "Compiling tests:"
-	echo "$TEST_CMD"
-	eval "$TEST_CMD"
 	# echo "Compiling header:"
 	echo "$HEADER_CMD"
 	eval "$HEADER_CMD"
 	exit 0
 else
-	export FINALCMD="concurrently --kill-others --prefix \"[{name}]\" --names \"SRC_,TEST,HEAD\""
-	FINALCMD="$FINALCMD -c \"bgGreen.black,bgBlue,bgYellow.black\""
+	export FINALCMD="concurrently --kill-others --prefix \"[{name}]\" --names \"SRC_,HEAD\""
+	FINALCMD="$FINALCMD -c \"bgGreen.black,bgYellow.black\""
 
 	SRC_CMD=`echo "$SRC_CMD" | sed -e 's/\"/\\\"/g'`
-	TEST_CMD=`echo "$TEST_CMD" | sed -e 's/\"/\\\"/g'`
 	HEADER_CMD=`echo "$HEADER_CMD" | sed -e 's/\"/\\\"/g'`
 
 	echo $SRC_CMD
-	echo $TEST_CMD
 	echo $HEADER_CMD
 
-	FINALCMD="$FINALCMD \"$SRC_CMD\" \"$TEST_CMD\" \"$HEADER_CMD\" "
+	FINALCMD="$FINALCMD \"$SRC_CMD\" \"$HEADER_CMD\" "
 	eval "$FINALCMD"
 fi
-
